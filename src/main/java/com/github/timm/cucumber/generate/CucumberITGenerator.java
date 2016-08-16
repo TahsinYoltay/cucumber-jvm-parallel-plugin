d4d8fa12b1924ba09d5d73c2d509f6d261314f99
robertgates55 <robertgates55@gmail.com>
2016-08-16 16:01:13 +0100

    updated pom

--- src/main/java/com/github/timm/cucumber/generate/CucumberITGenerator.java
+++ src/main/java/com/github/timm/cucumber/generate/CucumberITGenerator.java
@@ -8,6 +8,8 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
 import java.io.File;
 import java.io.FileWriter;
